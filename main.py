b6b7dde5670721e709ec6565c3f7663687ea85cc
Salvador <47413312+foodeater2@users.noreply.github.com>
2023-09-22 15:29:18 +0200

    main setup

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 #dette er hovedfilen til spillet, alle python filer blir importert hit.
+import python.spiller
+import python.rom
+import python.fiende
+
+#spiller = spiller.spiller()
+print("Velkommen til veldig kult spill 123!")
+
+while(True):
+
+    pass
