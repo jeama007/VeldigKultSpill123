59b7bbea1c408bef59134afb64d3f643aa1c9460
sunnivaschj <114073989+sunnivaschj@users.noreply.github.com>
2023-09-27 11:00:19 +0200

    Lagde en kommentar til konstruktøren

--- python/rom.py
+++ python/rom.py
@@ -2,6 +2,9 @@
 
 class Rom:
 
-    def __init__(self, felle, sporsmal):
+    # Lager en konstuktør som tar inn romnummer, feller og spørsmål
+    def __init__(self,romnummer, felle, sporsmal):
+        self.romnummer = romnummer
         self.felle = felle
         self.sporsmal = sporsmal
+
