3ba5bb605f6e13fc415c87f9bb908203bde602a4
sunnivaschj <114073989+sunnivaschj@users.noreply.github.com>
2023-09-27 11:04:05 +0200

    mer i kommentaren

--- python/rom.py
+++ python/rom.py
@@ -2,9 +2,11 @@
 
 class Rom:
 
-    # Lager en konstuktør som tar inn romnummer, feller og spørsmål
-    def __init__(self,romnummer, felle, sporsmal):
-        self.romnummer = romnummer
-        self.felle = felle
-        self.sporsmal = sporsmal
+    # Lager en konstuktør som tar inn roomnumber, trap og quiz. Quiz tenkte jeg kunne være spørsmål man får når man går inn i et nytt rom
+    def __init__(self,roomnumber, trap, quiz):
+        self.roomnumber = roomnumber
+        self.trap = trap
+        self.quiz = quiz
+    
+
 
