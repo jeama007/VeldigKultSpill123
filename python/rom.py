5dd3b298074a6d849b2b645bb99a2c4eab88c89b
sunnivaschj <114073989+sunnivaschj@users.noreply.github.com>
2023-09-27 11:09:14 +0200

    Litt mer på class quiz

--- python/rom.py
+++ python/rom.py
@@ -3,10 +3,16 @@
 class Rom:
 
     # Lager en konstuktør som tar inn roomnumber, trap og quiz. Quiz tenkte jeg kunne være spørsmål man får når man går inn i et nytt rom
-    def __init__(self,roomnumber, trap, quiz):
+    def __init__(self,roomnumber, trap, spørsmål):
         self.roomnumber = roomnumber
         self.trap = trap
-        self.quiz = quiz
+        self.spørsmål = spørsmål
     
+
+class Quiz:
+
+    def __init__(self, respons, kategori):
+        self.respons = respons
+        self.kategori = kategori
 
 
